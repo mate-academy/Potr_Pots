b2fd49a743d27bec85c52d32ca1bd1b959ca59c7
uni-8 <julia.zhol.a@gmail.com>
2020-09-07 00:22:07 +0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const popUp = document.querySelector('.pop-up');
+const buyButton = document.querySelectorAll('.buy-button');
+const closeButton = document.querySelector('.pop-up__close-button');
+const basket = document.querySelector('.header__basket');
+const basketMobile = document.querySelector('.basket-mobile');
+
+buyButton.forEach(item => item.addEventListener('click', openPopUp));
+closeButton.addEventListener('click', closePopp);
+basket.addEventListener('click', openPopUp);
+basketMobile.addEventListener('click', openPopUp);
+
+function openPopUp() {
+  popUp.style.cssText = `
+    z-index: 50;
+    opacity: 1;
+  `;
+}
+
+function closePopp() {
+  popUp.style.cssText = `
+    z-index: -5;
+    opacity: 0;
+  `;
+}
