be25e2b022c628be488d37b41155a140c25a4cac
uni-8 <julia.zhol.a@gmail.com>
2020-09-09 17:55:32 +0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const popUp = document.querySelector('.pop-up');
-const buyButton = document.querySelectorAll('.buy-button');
-const closeButton = document.querySelector('.pop-up__close-button');
-const basket = document.querySelector('.header__basket');
-const basketMobile = document.querySelector('.basket-mobile');
+(function initPopUp() {
+  const popUp = document.querySelector('.pop-up');
+  const buyButton = document.querySelectorAll('.buy-button');
+  const closeButton = document.querySelector('.pop-up__close-button');
+  const basket = document.querySelector('.header__basket');
+  const basketMobile = document.querySelector('.basket-mobile');
 
-buyButton.forEach(item => item.addEventListener('click', openPopUp));
-closeButton.addEventListener('click', closePopp);
-basket.addEventListener('click', openPopUp);
-basketMobile.addEventListener('click', openPopUp);
+  buyButton.forEach(item => item.addEventListener('click', openPopUp));
+  closeButton.addEventListener('click', closePopp);
+  basket.addEventListener('click', openPopUp);
+  basketMobile.addEventListener('click', openPopUp);
 
-function openPopUp() {
-  popUp.style.cssText = `
+  function openPopUp() {
+    popUp.style.cssText = `
     z-index: 50;
     opacity: 1;
   `;
-}
+  }
 
-function closePopp() {
-  popUp.style.cssText = `
+  function closePopp() {
+    popUp.style.cssText = `
     z-index: -5;
     opacity: 0;
   `;
-}
+  }
+})();
