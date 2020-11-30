867045786eb77425a85803acb6660ead6d083121
Dmytro Tsehelnyk <34zimnii@gmail.com>
2020-11-30 12:50:37 +0200

    responsive nav and smooth scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,51 @@
 'use strict';
+
+// show/hide navigation
+const navShow = document.querySelector('.js-nav-show');
+const navHide = document.querySelector('.js-nav-hide');
+const navMenu = document.querySelector('#navMenu');
+const navMenuClasses = navMenu.classList;
+
+navShow.addEventListener('click', () => {
+  if (!navMenuClasses.contains('nav--active')) {
+    navMenuClasses.add('nav--active');
+  }
+});
+
+navHide.addEventListener('click', () => {
+  if (navMenuClasses.contains('nav--active')) {
+    navMenuClasses.remove('nav--active');
+  }
+});
+
+document.querySelector('#features').scrollIntoView({
+  behavior: 'smooth',
+});
+
+// smooth scroll
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $('a').on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top,
+      }, 800, function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// parallax
+if (document.documentElement.clientWidth > 1024) {
+  const scene = document.getElementById('jsParallaxScene');
+  const parallax = new Parallax(scene);
+}
