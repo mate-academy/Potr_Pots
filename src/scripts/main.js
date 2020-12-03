051fb9429dc5b64ab8c295a754478591218a7931
Layrovell <marina95prodan@gmail.com>
2020-12-03 23:55:28 +0200

    fixes after a code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const cartBtn = document.querySelector('.header__cart');
+const openCart = document.querySelector('.shopping-cart');
+
+cartBtn.onclick = function() {
+  openCart.classList.toggle('shopping-cart--disabled');
+  openCart.classList.toggle('shopping-cart--move');
+};
+
+const closeBtn = document.querySelector('.close-btn');
+
+closeBtn.onclick = function() {
+  openCart.classList.remove('shopping-cart--move');
+  openCart.classList.add('shopping-cart--disabled');
+};
