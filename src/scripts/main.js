64fec2913cfc3d07cac53603d1372aceb87ed14f
5Mountains <V.Piatyhor@gmail.com>
2020-12-03 14:13:20 +0200

    added header & hero sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const burgerBth = document.querySelector('.header__burger-button');
+const headerNav = document.querySelector('.nav');
+const navMenu = document.querySelector('.nav__list');
+const closerBth = document.querySelector('.nav__x-close');
+
+burgerBth.addEventListener('click', (e) => {
+  headerNav.classList.toggle('nav--active');
+  navMenu.classList.toggle('nav__list--active');
+});
+
+closerBth.addEventListener('click', (e) => {
+  headerNav.classList.toggle('nav--active');
+  navMenu.classList.toggle('nav__list--active');
+});
+
+window.addEventListener('resize', (e) => {
+  if (window.innerWidth > 1025) {
+    headerNav.classList.remove('nav--active');
+    navMenu.classList.remove('nav__list--active');
+  }
+});
