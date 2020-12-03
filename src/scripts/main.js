e2b1c7742b7f06f9936d1859901d54ecbba7e6e3
Lora Levitska <larakad94@gmail.com>
2020-12-03 14:18:15 +0200

    add toggle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger');
+const toggler = document.querySelector('.burger__menu');
+
+const close = document.querySelector('.burger-open__close');
+
+toggler.onclick = function toggleSidebar() {
+  burger.classList.toggle('nav__burger--open');
+};
+
+close.onclick = function toggleSidebar() {
+  burger.classList.toggle('nav__burger--open');
+};
