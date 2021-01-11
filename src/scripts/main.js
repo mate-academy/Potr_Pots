00553b65d9c448f282568d8e1483571824d027a8
Lora Levitska <larakad94@gmail.com>
2021-01-11 16:58:20 +0200

    fix toggle materials moreInfo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,3 +12,12 @@ toggler.onclick = function toggleSidebar() {
 close.onclick = function toggleSidebar() {
   burger.classList.toggle('nav__burger--open');
 };
+
+const moreAboutmaterials = document.querySelector('.materials__buttons');
+const boxInfo = document.querySelector('.out');
+
+moreAboutmaterials.addEventListener('click', elem => {
+  if (elem.target.classList.contains('button')) {
+    boxInfo.classList.toggle('open');
+  }
+});
