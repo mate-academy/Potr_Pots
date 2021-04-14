abc8e2f30adb96ae781c4b62643b1544881223b6
Provolovskyi Illia <provolovskyi.illia@gmail.com>
2021-04-14 17:42:07 +0300

    potr-pots__001

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const title = document.querySelector('.nav');
+const button = document.querySelector('.header__toggler');
+const background = document.querySelector('.page-home__background');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+  title.classList.toggle('nav--active');
+  button.classList.toggle('header__toggler--active');
+  background.classList.toggle('page-home__background--active');
+});
