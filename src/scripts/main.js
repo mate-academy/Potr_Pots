d21871a5be6a9f80833b463576a7db18f7ada86e
Timur <hrynenko.timur@gmail.com>
2021-04-19 01:14:19 +0300

    solution added, cart modal added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const headerToggleBtn = document.querySelector('.header__menuToggler');
+const nav = document.querySelector('.nav');
+const menuCloseBtn = document.querySelector('.nav__closeBtn');
+const overlay = document.querySelector('.overlay');
+const page = document.querySelector('.page');
+
+const shoppingBagModal = document.querySelector('.shoppingBagModal');
+const shoppingBagIcon = document.querySelector('.header__cart');
+const shoppingBagCloseBtn = document
+  .querySelector('.shoppingBagModal__closeBtn');
+
+function navToggle() {
+  nav.classList.toggle('nav_active');
+  overlay.classList.toggle('overlay_active');
+  page.classList.toggle('page_no-scroll');
+}
+
+headerToggleBtn.addEventListener('click', navToggle);
+overlay.addEventListener('click', navToggle);
+menuCloseBtn.addEventListener('click', navToggle);
+
+function shoppingBagToggler() {
+  shoppingBagModal.classList.toggle('shoppingBagModal_active');
+  overlay.classList.toggle('overlay_active');
+}
+
+shoppingBagIcon.addEventListener('click', shoppingBagToggler);
+shoppingBagCloseBtn.addEventListener('click', shoppingBagToggler);
