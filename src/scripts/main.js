184b444cdc5247c2865200c09d932ae4dcd1412d
Sergei Trofimov <feast.st@gmail.com>
2021-06-09 21:36:15 +0300

    Added responsive header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menuBtn = document.getElementById('header__menu-btn');
+const headerClose = document.getElementById('nav');
+const menuCloseBtn = document.getElementById('nav__close-btn');
+
+menuBtn.onclick = function(event) {
+  headerClose.classList.add('nav--active');
+};
+
+menuCloseBtn.onclick = function(event) {
+  headerClose.classList.remove('nav--active');
+};
