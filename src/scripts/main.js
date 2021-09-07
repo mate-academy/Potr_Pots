3e0cc47b017ff5c333a1bb3a2cc2092a7a6e3181
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-09-07 09:10:55 +0300

    1-3 first draw

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const header = document.getElementById('header-top');
+const menuOpen = document.getElementById('header-menu-open');
+const menuClose = document.getElementById('header-menu-close');
+
+const headerClose = () => {
+  header.classList.remove('header--open');
+};
+
+menuOpen.addEventListener('click', () => {
+  header.classList.add('header--open');
+});
+
+menuClose.addEventListener('click', headerClose);
+
+document.querySelectorAll('.nav__item').forEach(li => {
+  li.addEventListener('click', headerClose);
+});
