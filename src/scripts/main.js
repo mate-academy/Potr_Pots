2f5f9c2c39a552c0a371ac6c98b11fd843b64238
Marina Tilniak <marinatilnak@gmail.com>
2021-12-09 03:08:08 +0200

    All except animation and side text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.contacts__form');
+const inputs = document.querySelectorAll('.contacts__field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(elem => {
+    elem.value = '';
+  });
+});
