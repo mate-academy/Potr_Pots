fdaa93834fcfac588817c41673407841a6f7f8e7
Mykola Koval <zavul0n.ua@gmail.com>
2021-12-14 08:42:10 +0200

    update 14/12

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.questions__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
