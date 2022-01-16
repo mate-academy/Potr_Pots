9e2d0c5d701be76b0adfa306f9a4b83de9601205
Dima Krasnoboky <krasnoboky.dima@gmail.com>
2022-01-17 01:00:50 +0200

    25%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const burger = document.querySelector('.header__burger');
+const menuClose = document.querySelector('.menu__close');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--active');
+});
+
+const menuItems = document.querySelectorAll('.menu__link');
+
+for (const item of menuItems) {
+  item.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+  });
+}
