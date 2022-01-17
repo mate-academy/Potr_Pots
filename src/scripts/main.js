c785e64a68f0598e0b0d7a3ccc96ab1e130d9add
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-17 18:37:11 +0200

    almost buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const materialsButtons = document.querySelectorAll('.materials__button');
+const materialsButtonsInfo = document.querySelectorAll('.materials__info');
+
+materialsButtonsInfo.forEach(e => {
+  e.classList.add('materials__info--hidden');
+});
+
+for (let i = 0; i < materialsButtons.length; i++) {
+  materialsButtons[i].addEventListener('click', () => {
+    materialsButtons[i].classList.toggle('materials__button--active');
+    materialsButtonsInfo[i].classList.toggle('materials__info--hidden');
+  });
+}
+
 const swiper = new Swiper('.mySwiper', {
   pagination: {
     el: '.swiper-pagination',
