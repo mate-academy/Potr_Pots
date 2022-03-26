8b4d19cc8e1e09eb9d81102a0768e0e7e9a5f0c1
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-03-26 17:35:38 +0200

    Create sections: questions, footer. Add hover effects. Refactor code of all sections. All is done except slider in mobile materials section.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#burger-menu') {
+    document.body.classList.add('page__body--scroll-off');
+  } else {
+    document.body.classList.remove('page__body--scroll-off');
+  }
+});
