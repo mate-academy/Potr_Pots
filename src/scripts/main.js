07ebd5c48eb12260737cb4d946818a48835b2fc2
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-03-28 15:29:30 +0300

    Add mobile touch slider to materials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// Swiper
+const swiper = new Swiper('.swiper', { // eslint-disable-line
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+// Page scroll off
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#burger-menu') {
     document.body.classList.add('page__body--scroll-off');
