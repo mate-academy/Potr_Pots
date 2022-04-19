a739a26962979c870e3757161f83963a6e28acac
Andriyk0 <andriy22221@gmail.com>
2022-04-19 18:40:20 +0300

    create 5 block, in last block use js

---
+++ src/main.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const cirkle1 = document.querySelector('.material__slider-circle-1');
+const slider1 = document.querySelector('.material__slider-1');
+const cirkle2 = document.querySelector('.material__slider-circle-2');
+const slider2 = document.querySelector('.material__slider-2');
+const cirkle3 = document.querySelector('.material__slider-circle-3');
+const slider3 = document.querySelector('.material__slider-3');
+const cirkle4 = document.querySelector('.material__slider-circle-4');
+const slider4 = document.querySelector('.material__slider-4');
+const cirkle5 = document.querySelector('.material__slider-circle-5');
+const slider5 = document.querySelector('.material__slider-5');
+
+cirkle1.addEventListener('click', function() {
+  this.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  this.textContent = '-';
+  this.style.lineHeight = '43px';
+  slider1.style.display = 'block';
+  cirkle2.style.backgroundColor = '#eb5757';
+  cirkle2.textContent = '+';
+  cirkle2.style.lineHeight = '';
+  slider2.style.display = 'none';
+  cirkle3.style.backgroundColor = '#eb5757';
+  cirkle3.textContent = '+';
+  cirkle3.style.lineHeight = '';
+  slider3.style.display = 'none';
+  cirkle4.style.backgroundColor = '#eb5757';
+  cirkle4.textContent = '+';
+  cirkle4.style.lineHeight = '';
+  slider4.style.display = 'none';
+  cirkle5.style.backgroundColor = '#eb5757';
+  cirkle5.textContent = '+';
+  cirkle5.style.lineHeight = '';
+  slider5.style.display = 'none';
+});
+
+cirkle2.addEventListener('click', function() {
+  this.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  this.textContent = '-';
+  this.style.lineHeight = '43px';
+  slider2.style.display = 'block';
+  cirkle1.style.backgroundColor = '#eb5757';
+  cirkle1.textContent = '+';
+  cirkle1.style.lineHeight = '';
+  slider1.style.display = 'none';
+  cirkle3.style.backgroundColor = '#eb5757';
+  cirkle3.textContent = '+';
+  cirkle3.style.lineHeight = '';
+  slider3.style.display = 'none';
+  cirkle4.style.backgroundColor = '#eb5757';
+  cirkle4.textContent = '+';
+  cirkle4.style.lineHeight = '';
+  slider4.style.display = 'none';
+  cirkle5.style.backgroundColor = '#eb5757';
+  cirkle5.textContent = '+';
+  cirkle5.style.lineHeight = '';
+  slider5.style.display = 'none';
+});
+
+cirkle3.addEventListener('click', function() {
+  this.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  this.textContent = '-';
+  this.style.lineHeight = '43px';
+  slider3.style.display = 'block';
+  cirkle1.style.backgroundColor = '#eb5757';
+  cirkle1.textContent = '+';
+  cirkle1.style.lineHeight = '';
+  slider1.style.display = 'none';
+  cirkle2.style.backgroundColor = '#eb5757';
+  cirkle2.textContent = '+';
+  cirkle2.style.lineHeight = '';
+  slider2.style.display = 'none';
+  cirkle4.style.backgroundColor = '#eb5757';
+  cirkle4.textContent = '+';
+  cirkle4.style.lineHeight = '';
+  slider4.style.display = 'none';
+  cirkle5.style.backgroundColor = '#eb5757';
+  cirkle5.textContent = '+';
+  cirkle5.style.lineHeight = '';
+  slider5.style.display = 'none';
+});
+
+cirkle4.addEventListener('click', function() {
+  this.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  this.textContent = '-';
+  this.style.lineHeight = '43px';
+  slider4.style.display = 'block';
+  cirkle1.style.backgroundColor = '#eb5757';
+  cirkle1.textContent = '+';
+  cirkle1.style.lineHeight = '';
+  slider1.style.display = 'none';
+  cirkle2.style.backgroundColor = '#eb5757';
+  cirkle2.textContent = '+';
+  cirkle2.style.lineHeight = '';
+  slider2.style.display = 'none';
+  cirkle3.style.backgroundColor = '#eb5757';
+  cirkle3.textContent = '+';
+  cirkle3.style.lineHeight = '';
+  slider3.style.display = 'none';
+  cirkle5.style.backgroundColor = '#eb5757';
+  cirkle5.textContent = '+';
+  cirkle5.style.lineHeight = '';
+  slider5.style.display = 'none';
+});
+
+cirkle5.addEventListener('click', function() {
+  this.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  this.textContent = '-';
+  this.style.lineHeight = '43px';
+  slider5.style.display = 'block';
+  cirkle1.style.backgroundColor = '#eb5757';
+  cirkle1.textContent = '+';
+  cirkle1.style.lineHeight = '';
+  slider1.style.display = 'none';
+  cirkle2.style.backgroundColor = '#eb5757';
+  cirkle2.textContent = '+';
+  cirkle2.style.lineHeight = '';
+  slider2.style.display = 'none';
+  cirkle3.style.backgroundColor = '#eb5757';
+  cirkle3.textContent = '+';
+  cirkle3.style.lineHeight = '';
+  slider3.style.display = 'none';
+  cirkle4.style.backgroundColor = '#eb5757';
+  cirkle4.textContent = '+';
+  cirkle4.style.lineHeight = '';
+  slider4.style.display = 'none';
+});
