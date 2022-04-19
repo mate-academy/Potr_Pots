10f651f327dc4eed878675f8d09c6675c689507f
efdalyalcin <efdalyalcin@hotmail.com>
2022-04-19 13:48:17 +0300

    slight adjustments allover the page is made, min-width removed. Questions section is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+/* window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__header--with-menu');
+  } else {
+    document.body.classList.remove('page__header--with-menu');
+  }
+}); */
+
+const formClick = document.getElementById('questions-form');
+
+formClick.addEventListener('submit', submit);
+
+function submit(e) {
+  e.preventDefault();
+  formClick.reset();
+  window.scrollTo(0, 0);
+}
