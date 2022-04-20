8c0a4e93d53fc0fbd480723037ee07bdbde896d4
Hubert Wolak <29hihub@gmail.com>
2022-04-20 19:46:59 +0200

    99% finished in my opinion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const button = document.querySelector('.header__burger-button');
+const menu = document.querySelector('.header__burger-menu');
+const closeB = document.querySelector('.burger-nav__close');
+
+button.addEventListener('click', function() {
+  menu.style.display = 'block';
+});
+
+closeB.addEventListener('click', function() {
+  menu.style.display = 'none';
+});
