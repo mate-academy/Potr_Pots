e5787dbc0c737e90c364826c30e1fe2cb8294faf
Hubert Wolak <29hihub@gmail.com>
2022-04-21 12:48:02 +0200

    Adjustment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,17 @@ button.addEventListener('click', function() {
 closeB.addEventListener('click', function() {
   menu.style.transform = 'translateX(75vw)';
 });
+
+const tx = document.getElementsByTagName('textArea');
+
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute('style', 'height:'
+  + (tx[i].scrollHeight)
+  + 'px;overflow-y:hidden;');
+  tx[i].addEventListener('input', OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+}
