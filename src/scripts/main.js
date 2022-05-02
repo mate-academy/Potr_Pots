c7f81295e7c97d1c1af661f0baf530ab8bd4215e
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-02 23:06:25 +0300

    +header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const icon = document.getElementById('header-burger');
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+const item4 = document.getElementById('item4');
+const item5 = document.getElementById('item5');
+const item6 = document.getElementById('item6');
+
+icon.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item1.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item2.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item3.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item4.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item5.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
+
+item6.addEventListener('click', () => {
+  document.getElementById('menu-container').classList.toggle('menu-opened');
+});
