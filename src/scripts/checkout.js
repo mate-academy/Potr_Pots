dee059cfc550792923c6f464244dd1b2b8b2e991
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-11 12:03:20 +0300

    +checkout

---
+++ src/scripts/checkout.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const card = document.getElementById('card');
+const paypal = document.getElementById('paypal');
+
+card.addEventListener('click', () => {
+  card.classList.add('checkout__card--active');
+  paypal.classList.remove('checkout__card--active');
+});
+
+paypal.addEventListener('click', () => {
+  paypal.classList.add('checkout__card--active');
+  card.classList.remove('checkout__card--active');
+});
+
+const button = document.getElementById('button1');
+
+button.addEventListener('click', () => {
+  document.querySelector('.checkout').classList.add('checkout--active');
+  document.querySelector('.basket').classList.remove('basket--active');
+  document.querySelector('.page').classList.remove('page--blured');
+});
+
+const coCross = document.querySelector('.checkout__cross');
+
+coCross.addEventListener('click', () => {
+  document.querySelector('.checkout').classList.remove('checkout--active');
+});
