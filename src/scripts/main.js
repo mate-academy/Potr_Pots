702deb868131a2701666a3756bb32efa76002cd2
Denys Halopa <galyopa@gmail.com>
2022-05-22 19:12:07 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const headerBtn = document.querySelector('.burger-btn');
+const headerBtnLine = document.querySelector('.burger-btn__line');
+const togglers = document.querySelectorAll('.nav__link, .burger-btn');
+// const pageBody = document.getElementById('home');
+
+togglers.forEach(toogler => {
+  toogler.addEventListener('click', () => {
+    // pageBody.classList.toggle('page__body--with-menu');
+    nav.classList.toggle('nav--active');
+    headerBtn.classList.toggle('burger-btn--active');
+    headerBtnLine.classList.toggle('burger-btn__line--active');
+  });
+});
