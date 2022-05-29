025beefcb4415e03a0e712d8af633c619c689941
Stas Yasinchyk <grodh06@gmail.com>
2022-05-29 22:19:33 +0300

    75% add tablet and desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const toggler1 = document.querySelector('#toggler1');
+const droptext1 = document.querySelector('#droptext1');
+
+toggler1.addEventListener('click', () => {
+  droptext1.classList.toggle('dropdown__droptext--hidden');
+  toggler1.classList.toggle('dropdown__toggler--open');
+});
+
+const toggler2 = document.querySelector('#toggler2');
+const droptext2 = document.querySelector('#droptext2');
+
+toggler2.addEventListener('click', () => {
+  droptext2.classList.toggle('dropdown__droptext--hidden');
+  toggler2.classList.toggle('dropdown__toggler--open');
+});
+
+const toggler3 = document.querySelector('#toggler3');
+const droptext3 = document.querySelector('#droptext3');
+
+toggler3.addEventListener('click', () => {
+  droptext3.classList.toggle('dropdown__droptext--hidden');
+  toggler3.classList.toggle('dropdown__toggler--open');
+});
+
+const toggler4 = document.querySelector('#toggler4');
+const droptext4 = document.querySelector('#droptext4');
+
+toggler4.addEventListener('click', () => {
+  droptext4.classList.toggle('dropdown__droptext--hidden');
+  toggler4.classList.toggle('dropdown__toggler--open');
+});
+
+const toggler5 = document.querySelector('#toggler5');
+const droptext5 = document.querySelector('#droptext5');
+
+toggler5.addEventListener('click', () => {
+  droptext5.classList.toggle('dropdown__droptext--hidden');
+  toggler5.classList.toggle('dropdown__toggler--open');
+});
