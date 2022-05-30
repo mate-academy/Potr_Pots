17caf7fbed01170e8d814b609ebe04afc7155537
Denys Halopa <galyopa@gmail.com>
2022-05-30 18:27:43 +0300

    add sprite.svg, form reset, fixed: bg footer, nav, body

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,21 @@ const nav = document.querySelector('.nav');
 const headerBtn = document.querySelector('.burger-btn');
 const headerBtnLine = document.querySelector('.burger-btn__line');
 const togglers = document.querySelectorAll('.nav__link, .burger-btn');
-// const pageBody = document.getElementById('home');
+const form = document.querySelector('.contact-form');
+const pageBody = document.getElementById('home');
 
 togglers.forEach(toogler => {
   toogler.addEventListener('click', () => {
-    // pageBody.classList.toggle('page__body--with-menu');
+    pageBody.classList.toggle('page__body--with-menu');
     nav.classList.toggle('nav--active');
     headerBtn.classList.toggle('burger-btn--active');
     headerBtnLine.classList.toggle('burger-btn__line--active');
   });
+});
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
 });
 
 new Splide('.splide', {
