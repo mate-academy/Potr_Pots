4aabe216816b2344f3ce20c74490ba458115a6bf
Andriy Hamernyk <0986758477and@gmail.com>
2022-05-31 10:57:11 +0300

    Project done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function submitForm() {
+  const frm = document.getElementsByName('contact-form')[0];
+
+  frm.submit();
+  frm.reset();
+}
+
+const btn = document.getElementsByClassName('contact__button');
+
+btn.addEventListener('click', submitForm());
