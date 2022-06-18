40730ce04c71f1e35f0cd7afa476b6c63ddca55f
Oleg Vetrov <oleg94038@gmail.com>
2022-06-18 11:05:34 +0300

    Added dropdown mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const nav = document.getElementById('nav');
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+});
