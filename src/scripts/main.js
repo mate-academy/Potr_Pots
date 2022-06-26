187d27f249b0a61c4ba2172679463f02b8fb0f89
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-27 01:06:22 +0300

    added lincs and opened menu mob

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,36 @@
 'use strict';
 
 const form = document.querySelector('.form');
+const pageElement = document.querySelector('.page');
+const menuMobOpenBtn = document.querySelector('.nav__menu-opened');
+const menuMob = document.querySelector('.nav-mob');
+const menuMobCloceBtn = document.querySelector('.nav-mob__btn-close');
+const navList = document.querySelector('.nav-mob__list');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   form.reset();
 });
+
+function handlerOpenMenu() {
+  menuMob.classList.add('nav-mob__open');
+  pageElement.classList.add('page--scroll-none');
+}
+
+function handlerCloceMenu() {
+  menuMob.classList.remove('nav-mob__open');
+  pageElement.classList.remove('page--scroll-none');
+}
+
+function handlerNavList(event) {
+  const { tagName } = event.target;
+
+  if (tagName === 'A') {
+    menuMob.classList.remove('nav-mob__open');
+    pageElement.classList.remove('page--scroll-none');
+  }
+}
+menuMobOpenBtn.addEventListener('click', handlerOpenMenu);
+menuMobCloceBtn.addEventListener('click', handlerCloceMenu);
+navList.addEventListener('click', handlerNavList);
