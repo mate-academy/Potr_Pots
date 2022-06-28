5b62ed885176189dae85de952a834e951a497aed
Olha Stakhova <olha.stakhova@gmail.com>
2022-06-29 00:11:07 +0300

    added slider in block materials

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,12 @@ const menuMobCloceBtn = document.querySelector('.nav-mob__btn-close');
 const navList = document.querySelector('.nav-mob__list');
 const backCloseElement = document.querySelector('.header__background-nav-mob');
 const materialsBtnArr = document.querySelectorAll('.materials__card-btn');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.modal__btn-close');
+const btnOpenModalHead = document.querySelector('.header__btn-buy');
+const btnOpenModBenef = document.querySelector('.benefits__btn-buy');
+const btnOpenBascetMob = document.querySelector('.nav-mob__btn-bascet');
+const btnOpenBascetDesc = document.querySelector('.nav__bascet');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -42,6 +48,16 @@ function handlerOpenDescriptMaterials(event) {
   event.currentTarget.classList.toggle('materials__btn-minus');
 }
 
+function handlerCloseModal() {
+  modal.classList.remove('modal__open');
+  pageElement.classList.remove('page--scroll-none');
+}
+
+function handlerOpenModalBascet() {
+  modal.classList.add('modal__open');
+  pageElement.classList.add('page--scroll-none');
+}
+
 menuMobOpenBtn.addEventListener('click', handlerOpenMenu);
 menuMobCloceBtn.addEventListener('click', handlerCloceMenu);
 navList.addEventListener('click', handlerNavList);
@@ -50,3 +66,9 @@ backCloseElement.addEventListener('click', handlerCloceMenu);
 materialsBtnArr.forEach((el) => {
   el.addEventListener('click', handlerOpenDescriptMaterials);
 });
+
+btnCloseModal.addEventListener('click', handlerCloseModal);
+btnOpenModBenef.addEventListener('click', handlerOpenModalBascet);
+btnOpenModalHead.addEventListener('click', handlerOpenModalBascet);
+btnOpenBascetMob.addEventListener('click', handlerOpenModalBascet);
+btnOpenBascetDesc.addEventListener('click', handlerOpenModalBascet);
