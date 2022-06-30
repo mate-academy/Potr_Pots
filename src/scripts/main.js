5d763b53ccc878abc056b70e5e5b6c6ea7aa105f
Yura Lysenko <yuralysenko1@gmail.com>
2022-06-30 16:10:48 +0300

    form-autofill and materials fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const formSubmit = document.getElementById('form');
+
+formSubmit.addEventListener('submit', (e) => {
+  document.getElementById('form').reset();
+  document.getElementById('form-submit').innerHTML = 'Submited!';
+  e.preventDefault();
+});
+
+const btn = document.getElementById('btn');
+const acordion = document.getElementById('acordion');
+
+btn.addEventListener('click', () => {
+  acordion.classList.toggle('wrapper--hidden');
+  
+});
