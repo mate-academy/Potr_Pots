a98556911802a8c0cc8ebf63a769b83827653e3d
Ihor Andrianov <andrianoia@gmail.com>
2022-08-31 18:31:53 +0300

    feat: add features, header, hero, team blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const button = document.querySelector('.burger-button');
+
+const menu = document.querySelector('.header__nav');
+const menuClasses = menu.classList;
+
+const headerCloseButton = document.querySelector('.header__close-button');
+
+headerCloseButton.addEventListener('click', () => {
+  menuClasses.toggle('active');
+});
+
+button.addEventListener('click', () => {
+  menuClasses.toggle('active');
+});
