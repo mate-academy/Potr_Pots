f1297869a09e59d937baab5a0710a06b6a2ccd35
Ihor Andrianov <andrianoia@gmail.com>
2022-09-01 11:30:58 +0300

    feat: add material section and slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,50 @@ headerCloseButton.addEventListener('click', () => {
 button.addEventListener('click', () => {
   menuClasses.toggle('active');
 });
+
+const sliderNav = document.querySelectorAll('.slider__navlink');
+
+window.addEventListener('hashchange', () => {
+  switch (window.location.hash) {
+    case '#slides__1':
+      sliderNav.forEach((e) => e.classList.remove('slider__active'));
+      sliderNav[0].classList.add('slider__active');
+      break;
+    case '#slides__2':
+      sliderNav.forEach((e) => e.classList.remove('slider__active'));
+      sliderNav[1].classList.add('slider__active');
+      break;
+    case '#slides__3':
+      sliderNav.forEach((e) => e.classList.remove('slider__active'));
+      sliderNav[2].classList.add('slider__active');
+      break;
+    case '#slides__4':
+      sliderNav.forEach((e) => e.classList.remove('slider__active'));
+      sliderNav[3].classList.add('slider__active');
+      break;
+    case '#slides__5':
+      sliderNav.forEach((e) => e.classList.remove('slider__active'));
+      sliderNav[4].classList.add('slider__active');
+      break;
+  }
+
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const sliderButton = document.querySelectorAll('.slider__button');
+const sliderSlide = document.querySelectorAll('.slider__slide');
+
+sliderButton.forEach((a, i) => {
+  a.addEventListener('click', () => {
+    sliderButton.forEach((e, index) =>
+      i === index
+        ? e.classList.toggle('button--active')
+        : e.classList.toggle('button--invis')
+    );
+    sliderSlide[i].classList.toggle('slider__active');
+  });
+});
