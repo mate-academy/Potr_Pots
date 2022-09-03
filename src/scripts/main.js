24da64da59df5d8ba3c13a594d0ab4a059f29150
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-09-04 00:59:40 +0300

    Unify repeating styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,9 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+// function showPopUp() {
+//   const popUpText = document.getElementById('advantage-1');
+
+//   popUpText.classList.toggle('show');
+// }
