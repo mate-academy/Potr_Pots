cd6a7c8446d541783d536bddce8e48fc0d8785a0
Oleh Bazyla <olegbazila@gmail.com>
2022-09-05 18:58:04 +0300

    Header is fully adapted, added readme file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function clearHash() {
+  const uri = window.location.toString();
+
+  if (uri.indexOf('#') > 0) {
+    const cleanUri = uri.substring(0, uri.indexOf('#'));
+
+    window.history.replaceState({}, document.title, cleanUri);
+  }
+}
+
+window.addEventListener('hashchange', () => {
+  const menuBox = document.getElementById('menu');
+
+  if (window.location.hash === '#menu') {
+    menuBox.classList.add('page__menu--open');
+    menuBox.classList.remove('page__menu--close');
+  } else {
+    menuBox.classList.add('page__menu--close');
+    menuBox.classList.remove('page__menu--open');
+  }
+});
+
+window.addEventListener('mouseup', (event) => {
+  const menuBox = document.getElementById('menu');
+
+  if (event.target === menuBox) {
+    menuBox.classList.add('page__menu--open');
+    menuBox.classList.remove('page__menu--close');
+  } else {
+    clearHash();
+    menuBox.classList.add('page__menu--close');
+    menuBox.classList.remove('page__menu--open');
+  }
+});
