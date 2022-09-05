3e7218bc8072d3b90b2355d31eac9198c6e8bc3f
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-05 17:30:03 +0300

    Added header and menu for phones

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menuOpenButton = document.querySelector('.header__menu-button');
+const menuCloseButton = document.querySelector('.menu__close');
+const menu = document.querySelector('.menu');
+
+menuOpenButton.addEventListener('click', () => {
+  menu.classList.add('menu--open');
+  menu.classList.remove('menu--close');
+});
+
+menuCloseButton.addEventListener('click', () => {
+  menu.classList.add('menu--close');
+  menu.classList.remove('menu--open');
+});
