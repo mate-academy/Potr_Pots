189410c77f440dc8674f1f4aa12f6a11c5179f72
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-06 18:51:08 +0300

    Added features section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,22 @@
 const menuOpenButton = document.querySelector('.header__menu-button');
 const menuCloseButton = document.querySelector('.menu__close');
 const menu = document.querySelector('.menu');
+const menuList = document.querySelector('.menu__list');
+let isMenuOpen = false;
 
 menuOpenButton.addEventListener('click', () => {
   menu.classList.remove('menu--close');
+  isMenuOpen = true;
 });
 
 menuCloseButton.addEventListener('click', () => {
   menu.classList.add('menu--close');
+  isMenuOpen = false;
 });
+
+if (!isMenuOpen) {
+  menuList.addEventListener('click', () => {
+    menu.classList.add('menu--close');
+    isMenuOpen = false;
+  });
+}
