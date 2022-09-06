5f033efac88ff1841efe922514e7e1a39f60fadc
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-06 15:09:02 +0300

    Added for whom section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,9 @@ const menuCloseButton = document.querySelector('.menu__close');
 const menu = document.querySelector('.menu');
 
 menuOpenButton.addEventListener('click', () => {
-  menu.classList.add('menu--open');
   menu.classList.remove('menu--close');
 });
 
 menuCloseButton.addEventListener('click', () => {
   menu.classList.add('menu--close');
-  menu.classList.remove('menu--open');
 });
