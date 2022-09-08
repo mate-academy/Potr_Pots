4761113ed456c74c1c35064485d0b5aadfb2aead
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-08 19:55:44 +0300

    Fixed slider

--- src/scripts/main-file.js
+++ src/scripts/main-file.js
@@ -27,33 +27,10 @@ const tabletButtons = document.querySelectorAll('.materials__tablet-button');
 
 tabletButtons.forEach(function(btn, index) {
   btn.addEventListener('click', () => {
-    const actBtn = document.querySelector('.materials__tablet-button--active');
-    const activeText = document.querySelector('.slider__text--is-active');
-    const slidersText = document.querySelector('.materials__slider');
+    const activeText = document.querySelectorAll('.slider__tb');
 
-    if (index === 4) {
-      slidersText.style.bottom = '18%';
-      slidersText.style.left = '63%';
-    } else if (index === 3) {
-      slidersText.style.bottom = '20%';
-      slidersText.style.left = '50%';
-    } else if (index === 2) {
-      slidersText.style.bottom = '10%';
-      slidersText.style.left = '36%';
-    } else if (index === 1) {
-      slidersText.style.bottom = '36%';
-      slidersText.style.left = '14%';
-    } else if (index === 0) {
-      slidersText.style.bottom = '15%';
-      slidersText.style.left = '5%';
-    }
-
-    activeText.classList.remove('slider__text--is-active');
-    text[index].classList.add('slider__text--is-active');
-    actBtn.classList.remove('materials__tablet-button--active');
-    actBtn.innerHTML = '+';
-    btn.innerHTML = '-';
-    btn.classList.add('materials__tablet-button--active');
+    activeText[index].classList.toggle('slider__tb--active');
+    btn.classList.toggle('materials__tablet-button--active');
   });
 });
 
