4ae128097b481e07d0db13df48d815fc203f0c59
Oleh Bazyla <olegbazila@gmail.com>
2022-09-09 06:06:47 +0300

    update index.html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-const form = document.querySelector('.form');
-const inputs = document.querySelectorAll('.form__input');
-
-function handler(event) {
-  event.preventDefault();
-
-  inputs.forEach(input => {
-    input.value = '';
-  });
-}
-
-form.addEventListener('submit', handler);
-
 function clearHash() {
   const uri = window.location.toString();
 
