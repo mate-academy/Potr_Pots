acdb16f546e7ee89e748f917b548472d330fd7e7
Halyna Rydasova <halyna.rydasova@gmail.com>
2022-11-11 15:55:32 +0200

    created materials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,46 @@ window.addEventListener('hash', () => {
     document.body.classList.remove('page__body--no-scroll');
   }
 });
+
+const width = window.screen.width;
+
+if (width < 768) {
+  showSlides(0);
+}
+
+const buttons = document.querySelectorAll('.slider__button');
+
+buttons.forEach((item, index) => {
+  item.addEventListener('click', (event) => {
+    currentSlide(index);
+  });
+});
+
+function currentSlide(n) {
+  showSlides(n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName('slider__item');
+  const dots = document.getElementsByClassName('slider__button');
+
+  Array.from(slides).forEach(function(item) {
+    item.classList.remove('slider__item--active');
+  });
+
+  Array.from(dots).forEach(function(item) {
+    item.classList.remove('slider__button--active');
+  });
+
+  slides[n].className += ' slider__item--active';
+  dots[n].className += ' slider__button--active';
+}
+
+const spoilers = document.querySelectorAll('.slider__spoiler');
+
+spoilers.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    item.classList.toggle('slider__spoiler--active');
+    item.previousElementSibling.classList.toggle('slider__description--active');
+  });
+});
