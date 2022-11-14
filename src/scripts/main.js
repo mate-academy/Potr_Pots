53aaa079ecf9e71615e05a015cb3e28f4fff3c7d
Denis Bokov <denisbokov1703@gmail.com>
2022-11-14 19:30:45 +0100

    POTR POTS (1)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const forms = document.querySelectorAll('.form__field');
+
+const formField = document.querySelector('.form');
+
+formField.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  forms.forEach((form) => {
+    form.value = '';
+  });
+});
