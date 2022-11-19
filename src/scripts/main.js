a7a6fb53c4109d99f0c939bdf49fcb498e1ab342
Demut <uddarendeli1@gmail.com>
2022-11-20 01:19:10 +0300

    Added support for touch events

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
+// Slider
 const openers = document.querySelectorAll('.modals__opener');
 const sliderpages = document.querySelectorAll('.slider__page');
 const controls = document.getElementById('mtrls-n-ftrs--controls');
+const slider = document.querySelector('.slider');
 
-const moveImage = (to) => {
+const moveImage = (to, action = 0) => {
   let from;
   const classList = controls.classList;
 
@@ -16,10 +18,12 @@ const moveImage = (to) => {
     }
   }
 
+  const To = to === -1 ? Number.parseInt(from) : to;
+
   controls.classList.remove('modals--selected-' + from);
-  controls.classList.add('modals--selected-' + to);
+  controls.classList.add('modals--selected-' + (To + action));
   controls.classList.remove('slider--selected-' + from);
-  controls.classList.add('slider--selected-' + to);
+  controls.classList.add('slider--selected-' + (To + action));
 };
 
 for (let i = 0; i < openers.length; i++) {
@@ -31,3 +35,44 @@ for (let i = 0; i < openers.length; i++) {
     moveImage(i + 1);
   });
 }
+
+slider.addEventListener('touchstart', handleTouchStart, false);
+slider.addEventListener('touchmove', handleTouchMove, false);
+
+// Touch event handlers
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches // browser API
+    || evt.originalEvent.touches; // jQuery
+};
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  const xUp = evt.touches[0].clientX;
+  const yUp = evt.touches[0].clientY;
+  const xDiff = xDown - xUp;
+  const yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) { // most significant
+    if (xDiff > 0) {
+      moveImage(-1, 1);
+    } else {
+      moveImage(-1, -1);
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+};
