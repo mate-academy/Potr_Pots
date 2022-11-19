4b0f21b7ceba43813b7ba5d265c699bd58e874e4
Demut <uddarendeli1@gmail.com>
2022-11-20 00:52:21 +0300

    Site links work correctly, slider and modal openers implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const openers = document.querySelectorAll('.modals__opener');
+const sliderpages = document.querySelectorAll('.slider__page');
+const controls = document.getElementById('mtrls-n-ftrs--controls');
+
+const moveImage = (to) => {
+  let from;
+  const classList = controls.classList;
+
+  for (let i = 0; i < classList.length; i++) {
+    from = classList[i].match(/(?<=--selected-)./g);
+
+    if (from) {
+      break;
+    }
+  }
+
+  controls.classList.remove('modals--selected-' + from);
+  controls.classList.add('modals--selected-' + to);
+  controls.classList.remove('slider--selected-' + from);
+  controls.classList.add('slider--selected-' + to);
+};
+
+for (let i = 0; i < openers.length; i++) {
+  openers[i].addEventListener('click', (e) => {
+    moveImage(i + 1);
+  });
+
+  sliderpages[i].addEventListener('click', (e) => {
+    moveImage(i + 1);
+  });
+}
