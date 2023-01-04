995d0c812afd51bdd8bad20473e85db105a5b089
Serhii Prikhodko <venom99@ukr.net>
2023-01-04 21:27:31 +0200

    JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,18 +18,57 @@ form.addEventListener('submit', function(event) {
 
 // popup
 
-const slide = document.querySelectorAll('.slide__list');
+// const slide = document.querySelectorAll('.slide__list');
 
-slide.forEach(element => {
-  element.addEventListener('click', (e) => {
-    e.preventDefault();
+// slide.forEach(element => {
+//   element.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    const item = e.target.closest('.slide__list-link');
+//     const item = e.target.closest('.slide__list-link');
 
-    item.classList.toggle('slide__list-link--close');
+//     item.classList.toggle('slide__list-link--close');
 
-    const description = e.target.nextElementSibling;
+//     const description = e.target.nextElementSibling;
 
-    description.classList.toggle('slide__list-description--open');
-  });
+//     description.classList.toggle('slide__list-description--open');
+//   });
+// });
+
+const list = document.querySelector('.materials__slide');
+// eslint-disable-next-line max-len
+const slideDescription = document.getElementsByClassName('slide__list-description');
+const slideLink = document.getElementsByClassName('slide__list-link');
+// let switchOver;
+
+list.addEventListener('click', event => {
+  event.preventDefault();
+
+  const item = event.target.closest('.slide__list-link');
+
+  if (!item || !list.contains(item)) {
+    return null;
+  }
+
+  const description = event.target.nextElementSibling;
+
+  // if (switchOver) {
+  // eslint-disable-next-line max-len
+  [...slideLink].forEach(link => link.classList.remove('slide__list-link--close'));
+  // eslint-disable-next-line max-len
+  [...slideDescription].forEach(descriptionItem => descriptionItem.classList.remove('slide__list-description--open'));
+  // switchOver = false;
+  // }
+
+  if (description.classList.contains('slide__list-description--open')) {
+    item.classList.remove('slide__list-link--close');
+    description.classList.remove('slide__list-description--open');
+  } else {
+    item.classList.add('slide__list-link--close');
+    description.classList.add('slide__list-description--open');
+  }
+
+  // item.classList.toggle('slide__list-link--close');
+  // description.classList.toggle('slide__list-description--open');
+
+  // switchOver = true;
 });
