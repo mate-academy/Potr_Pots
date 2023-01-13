04824052f695370c37dbc6384c376461b8a3b5ae
Matvii <admin@admins-MacBook-Air.local>
2023-01-13 20:10:29 +0100

    commit for gh

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const burger = document.querySelector('.header__burger');
+const closeNav = document.querySelector('.nav__close ');
+const nav = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.add('active-nav');
+  document.body.style.overflowY = 'hidden';
+});
+
+closeNav.addEventListener('click', () => {
+  nav.classList.remove('active-nav');
+  document.body.style.overflowY = 'visible';
+});
