cebb05a64757865540fbf9649d3028c21b9ebf2f
Natalia Tsiutsiura <89_22@ukr.net>
2023-03-27 22:14:50 +0300

    Final part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/* eslint-disable */
+var links = document.querySelectorAll('.menu a');
+var linksLength = links.length
+
+for(var i = 0; i < linksLength; i++) {
+  links[i].addEventListener('click', function() {
+    document.getElementById('header__vector').checked = false;
+  });
+}
+
 const pageReload = document.querySelector('#form');
 
 pageReload.addEventListener('submit', (event) => {
