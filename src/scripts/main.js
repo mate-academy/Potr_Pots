766fa7bd7c805392acd38be1e3eab81f554de777
Yura Enttsel <equikmla@gmail.com>
2023-03-30 16:21:40 +0300

    add footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const form = document.getElementById('form');
+
+function submitForm(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', submitForm);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
