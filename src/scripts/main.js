c8e3bfa4b54485def9c1d2cc8e36b0ebd0cc8588
Yura Enttsel <equikmla@gmail.com>
2023-03-30 21:55:06 +0300

    fixed buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const form = document.getElementById('form');
 
 function submitForm(event) {
@@ -9,10 +17,24 @@ function submitForm(event) {
 
 form.addEventListener('submit', submitForm);
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
+const buttons = document.querySelectorAll('.button-circle');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', function() {
+    const textBlockSelector = this.getAttribute('data-target');
+    const textBlock = document.getElementById(textBlockSelector);
+    const isActive = this.getAttribute('data-active');
+
+    if (isActive === 'false') {
+      textBlock.classList.add('active');
+      this.innerHTML = '&#8722;';
+      this.classList.add('active');
+      this.setAttribute('data-active', 'true');
+    } else {
+      textBlock.classList.remove('active');
+      this.innerHTML = '&#43;';
+      this.classList.remove('active');
+      this.setAttribute('data-active', 'false');
+    }
+  });
 });
