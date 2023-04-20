2ebe819f50370f3cae900b6ad05a255d67562022
Dmytro Fefelov <reseneweb@gmail.com>
2023-04-20 12:05:46 +0300

    fixed hover effect on size__photo and fixed sending form by using js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
