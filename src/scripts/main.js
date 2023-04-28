a5e7d282f2315b2e7ef359032a06f4276fc8268d
Volodymyr Shostak <shostak_1@proton.me>
2023-04-28 21:11:43 +0300

    add adjustments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,14 @@ const swiper = new Swiper('.materials__swiper', {
   },
 });
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', function(event) {
