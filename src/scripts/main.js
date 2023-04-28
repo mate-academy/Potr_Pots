793f897c479ed87f4b0d3c4f36393ddb0d98d117
Volodymyr Shostak <shostak_1@proton.me>
2023-04-28 16:51:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// const swiper = new Swiper('.materials__swiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+// });
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
