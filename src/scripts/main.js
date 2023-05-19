844577eccba74e2985589769300f4bf32429bcd9
denys2 <brownexpert@gmail.com>
2023-05-19 23:15:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else if (window.location.hash === '#language') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper('.materials__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'slide',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 18,
+
+  pagination: {
+    el: '.materials__pagination',
+    clickable: true,
+  },
+});
