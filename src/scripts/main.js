c8fbf2907d965d5b8e20c44d6dee42bb274fc20c
Valera Viachalo <valeravyachalo@gmail.com>
2023-06-01 13:56:04 +0300

    post port

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+/* global Swiper */
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.contact-us__form');
+
+form.addEventListener('submit', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+  form.reset();
+};
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+swiper.enable();
