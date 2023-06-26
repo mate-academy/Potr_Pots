e95db9262d78cae056f4fe66f017408ba3f97c10
Nikolai Melnichenko <nikolaymelni@gmail.com>
2023-06-26 18:52:21 +0300

    fixies, swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,43 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--no-scroll');
   }
 });
+
+const button1 = document.querySelector('.materials__button--1');
+
+button1.addEventListener('click', () => {
+  const element = document.querySelector('.materials__card--1');
+
+  element.classList.toggle('materials__card--active');
+});
+
+const button2 = document.querySelector('.materials__button--2');
+
+button2.addEventListener('click', () => {
+  const element = document.querySelector('.materials__card--2');
+
+  element.classList.toggle('materials__card--active');
+});
+
+const button3 = document.querySelector('.materials__button--3');
+
+button3.addEventListener('click', () => {
+  const element = document.querySelector('.materials__card--3');
+
+  element.classList.toggle('materials__card--active');
+});
+
+const button4 = document.querySelector('.materials__button--4');
+
+button4.addEventListener('click', () => {
+  const element = document.querySelector('.materials__card--4');
+
+  element.classList.toggle('materials__card--active');
+});
+
+const button5 = document.querySelector('.materials__button--5');
+
+button5.addEventListener('click', () => {
+  const element = document.querySelector('.materials__card--5');
+
+  element.classList.toggle('materials__card--active');
+});
