0224d9c7d0e3dfc297c03e1e5a4b8a62825f9cc9
Nikolai Melnichenko <nikolaymelni@gmail.com>
2023-06-29 17:27:35 +0300

    100%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,20 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  form.reset();
+});
+
 const button1 = document.querySelector('.materials__button--1');
 
 button1.addEventListener('click', () => {
   const element = document.querySelector('.materials__card--1');
 
   element.classList.toggle('materials__card--active');
+  button1.classList.toggle('materials__button--active');
 });
 
 const button2 = document.querySelector('.materials__button--2');
@@ -22,6 +30,7 @@ button2.addEventListener('click', () => {
   const element = document.querySelector('.materials__card--2');
 
   element.classList.toggle('materials__card--active');
+  button2.classList.toggle('materials__button--active');
 });
 
 const button3 = document.querySelector('.materials__button--3');
@@ -30,6 +39,7 @@ button3.addEventListener('click', () => {
   const element = document.querySelector('.materials__card--3');
 
   element.classList.toggle('materials__card--active');
+  button3.classList.toggle('materials__button--active');
 });
 
 const button4 = document.querySelector('.materials__button--4');
@@ -38,6 +48,7 @@ button4.addEventListener('click', () => {
   const element = document.querySelector('.materials__card--4');
 
   element.classList.toggle('materials__card--active');
+  button4.classList.toggle('materials__button--active');
 });
 
 const button5 = document.querySelector('.materials__button--5');
@@ -46,4 +57,5 @@ button5.addEventListener('click', () => {
   const element = document.querySelector('.materials__card--5');
 
   element.classList.toggle('materials__card--active');
+  button5.classList.toggle('materials__button--active');
 });
