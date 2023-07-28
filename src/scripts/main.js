681428731a3e0b481022ab5308e2d563a64b6634
Ivan <fanmetalheavy@gmail.com>
2023-07-28 16:23:03 +0300

    done mobile layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// $(document).ready(function() {
+//   $('.rhombuses-container__rhombus').click(function() {
+//     const index = $(this).data('index');
+
+//     $('.materials__slider-box').hide();
+
+//     $('.materials__slider-box--' + index).show();
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const sliderBoxes = document.querySelectorAll('.materials__slider-box');
+
+  for (let i = 1; i < sliderBoxes.length; i++) {
+    sliderBoxes[i].style.display = 'none';
+  }
+
+  const sliderList = document.querySelector('.materials__slider-list');
+  const hammer = new Hammer(sliderList);
+
+  let currentIndex = 0;
+
+  hammer.on('swipeleft swiperight', function(event) {
+    sliderBoxes[currentIndex].style.display = 'none';
+
+    if (event.type === 'swipeleft') {
+      currentIndex = (currentIndex + 1) % sliderBoxes.length;
+    } else if (event.type === 'swiperight') {
+      currentIndex = (currentIndex - 1 + sliderBoxes.length)
+        % sliderBoxes.length;
+    }
+
+    sliderBoxes[currentIndex].style.display = 'block';
+  });
+});
