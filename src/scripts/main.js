9837d133668f0d60dac0b777b7fc6cbe2847f969
Danylo Artomov <daniilart01@gmail.com>
2023-08-31 21:31:01 +0300

    header + for whom blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const link = document.getElementById('basket/menu');
+
+function updateLink() {
+  if (window.innerWidth < 1279) {
+    link.href = '#menu';
+  } else {
+    link.href = '#';
+  }
+}
+
+window.addEventListener('load', updateLink);
+window.addEventListener('resize', updateLink);
