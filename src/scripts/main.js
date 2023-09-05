40b07d34257b97e415f835c136436c1efac84038
Klaudia <kklis@interia.pl>
2023-09-05 12:51:44 +0200

    Resolve 6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// SLIDES
+
 function toggleSlide1() {
   const x = document.getElementById('slide-1');
 
@@ -94,3 +96,160 @@ function toggleSlide5() {
 
 document.querySelector('.materials__image--5')
   .addEventListener('click', toggleSlide5);
+
+// MODAL
+
+const modal = document.getElementById('basket__modal');
+const btn = document.getElementById('basket');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+  generateTable();
+};
+
+span.onclick = function() {
+  modal.style.display = 'none';
+
+  const table = document.getElementById('table');
+
+  table.remove();
+};
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+
+    const table = document.getElementById('table');
+
+    table.remove();
+  }
+};
+
+// BASKET
+
+const products = [{
+  id: 1,
+  name: 'Product 1',
+  prize: 10,
+},
+{
+  id: 2,
+  name: 'Product 2',
+  prize: 11,
+}];
+
+const basket = [];
+
+const product1 = document.getElementById('product-1');
+const product2 = document.getElementById('product-2');
+
+product1.onclick = function() {
+  const addedProduct = products[0];
+
+  basket.push(addedProduct);
+
+  setTimeout(showPopup1(), 1000);
+};
+
+product2.onclick = function() {
+  const addedProduct = products[1];
+
+  basket.push(addedProduct);
+
+  showPopup2();
+};
+
+const modalBody = document.querySelector('.modal-body');
+
+function generateTable() {
+  const paragraph = document.createElement('p');
+
+  paragraph.setAttribute('class', 'table__paragraph');
+
+  const content = basket.length === 0
+    ? document.createTextNode('Your basket is empty.')
+    : basket.length === 1
+      ? document.createTextNode(
+        `You have ${basket.length} item in your basket.`
+      )
+      : document.createTextNode(
+        `You have ${basket.length} items in your basket.`
+      );
+
+  paragraph.appendChild(content);
+
+  const tbl = document.createElement('table');
+
+  tbl.appendChild(paragraph);
+
+  const headName = document.createElement('td');
+  const name = document.createTextNode('Name');
+  const headPrize = document.createElement('td');
+  const prize = document.createTextNode('Prize');
+  const headRow = document.createElement('tr');
+
+  headName.appendChild(name);
+  headPrize.appendChild(prize);
+
+  headRow.appendChild(headName);
+  headRow.appendChild(headPrize);
+
+  tbl.setAttribute('id', 'table');
+
+  const tblBody = document.createElement('tbody');
+  let countPrize = 0;
+
+  if (basket.length > 0) {
+    tblBody.appendChild(headRow);
+  }
+
+  for (let i = 0; i < basket.length; i++) {
+    const row = document.createElement('tr');
+    const keys = Object.entries(basket[i]);
+
+    countPrize += basket[i].prize;
+
+    for (let j = 1; j < keys.length; j++) {
+      const cell = document.createElement('td');
+      const cellText = document.createTextNode(`${keys[j][1]}`);
+
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    tblBody.appendChild(row);
+  }
+
+  const totalPrizeRow = document.createElement('tr');
+  const totalPrizeData = document.createElement('td');
+  const totalPrize = document.createTextNode(`Total prize: ${countPrize} $`);
+
+  totalPrizeData.setAttribute('class', 'table__total');
+
+  totalPrizeData.appendChild(totalPrize);
+  totalPrizeRow.appendChild(totalPrizeData);
+  tblBody.appendChild(totalPrizeRow);
+  tbl.appendChild(tblBody);
+  modalBody.appendChild(tbl);
+}
+
+// POPUP
+const popup1 = document.getElementById('added-product-1');
+const popup2 = document.getElementById('added-product-2');
+
+function showPopup1() {
+  popup1.classList.add('show');
+
+  setTimeout(function() {
+    popup1.classList.remove('show');
+  }, 1200);
+};
+
+function showPopup2() {
+  popup2.classList.add('show');
+
+  setTimeout(function() {
+    popup2.classList.remove('show');
+  }, 1200);
+};
