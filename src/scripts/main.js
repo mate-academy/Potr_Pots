388895022d9b91360fc3ba0e4cac10333dd2f64b
Inna Sheremet <innsheremet88@gmail.com>
2023-09-10 16:57:34 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function addClassToHeader() {
+  const header = document.getElementById('header-content');
+
+  if (window.innerWidth >= 768) {
+    header.classList.add('container');
+  } else {
+    header.classList.remove('container');
+  }
+}
+
+addClassToHeader();
+window.addEventListener('resize', addClassToHeader);
+
+function removeClassIfScreenWidthIsSmall() {
+  const element = document.getElementById('header-title');
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth >= 768) {
+    element.classList.remove('container');
+  } else {
+    element.classList.add('container');
+  }
+}
+
+window.addEventListener('load', removeClassIfScreenWidthIsSmall);
+window.addEventListener('resize', removeClassIfScreenWidthIsSmall);
