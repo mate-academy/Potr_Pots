8425807781b36e65b74ac6622ea7d3380be27625
Inna Sheremet <innsheremet88@gmail.com>
2023-09-15 22:38:22 +0300

    add section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,53 @@
 'use strict';
 
-function addClassToHeader() {
+function changeClassToLargeScrins() {
   const header = document.getElementById('header-content');
+  const content = document.getElementById('meet-content');
+  const element = document.getElementById('header-title');
 
   if (window.innerWidth >= 768) {
     header.classList.add('container');
-  } else {
-    header.classList.remove('container');
-  }
-};
-
-addClassToHeader();
-window.addEventListener('resize', addClassToHeader);
-
-/* --------------------remove_container------------------------- */
-
-function removeClassIfScreenWidthIsSmall() {
-  const element = document.getElementById('header-title');
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth >= 768) {
+    content.classList.add('container');
     element.classList.remove('container');
   } else {
+    header.classList.remove('container');
+    content.classList.remove('container');
     element.classList.add('container');
   }
 };
 
-window.addEventListener('load', removeClassIfScreenWidthIsSmall);
-window.addEventListener('resize', removeClassIfScreenWidthIsSmall);
+changeClassToLargeScrins();
+window.addEventListener('resize', changeClassToLargeScrins);
+
+/* --------------------remove_container------------------------- */
+
+// function removeClassIfScreenWidthIsSmall() {
+//   const element = document.getElementById('header-title');
+//   const screenWidth = window.innerWidth;
+
+//   if (screenWidth >= 768) {
+//     element.classList.remove('container');
+//   } else {
+//     element.classList.add('container');
+//   }
+// };
+
+// window.addEventListener('load', removeClassIfScreenWidthIsSmall);
+// window.addEventListener('resize', removeClassIfScreenWidthIsSmall);
 
 /* --------------------------------------------------------------- */
 
-function addClassToMeet() {
-  const content = document.getElementById('meet-content');
+// function addClassToMeet() {
+//   const content = document.getElementById('meet-content');
 
-  if (window.innerWidth >= 768) {
-    content.classList.add('container');
-  };
-};
+//   if (window.innerWidth >= 768) {
+//     content.classList.add('container');
+//   } else {
+//     content.classList.remove('container');
+//   }
+// };
 
-addClassToHeader();
-window.addEventListener('resize', addClassToMeet);
+// addClassToHeader();
+// window.addEventListener('resize', addClassToMeet);
+
+/* ----------------------------slider---------------------------- */
