96f8d3106c43a460df27c6c6eb70c5d1eb7f2886
Anastasiia Yukhymenko <iamnastya25@gmail.com>
2023-09-23 02:49:53 +0300

    site added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+/* const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+}); */
+
+const materialsFeature = document.querySelectorAll('.materials__feature');
+
+materialsFeature.forEach((el) => {
+  el.addEventListener('click', () => {
+    el.classList.toggle('materials__feature--active');
+    el.children[0].classList.toggle('materials__feature-box--active');
+  });
+});
