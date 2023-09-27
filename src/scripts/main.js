ca5002dc4a55624ddbd1c69eaae46d1a9c106224
Oleksandr Kosenko <d1izel@ukr.net>
2023-09-27 15:00:03 +0300

    add landing page potr_pots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Slider
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.slider').slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  speed: 500,
+  easing: 'ease',
+  initialSlide: 0,
+  autoplay: false,
+  autoplaySpeed: 3000,
+});
+
+// Materials Popups Buttons
+
+const popupArr = ['one', 'two', 'three', 'four', 'five'];
+
+for (const i of popupArr) {
+  const btn = document.querySelector(`.popup__btn--${i}`);
+  const popup = document.querySelector(`.popup__inner--${i}`);
+
+  if (btn) {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      popup.classList.toggle('popup__inner--active');
+      btn.classList.toggle('materials__button--close');
+    })
+  }
+}
