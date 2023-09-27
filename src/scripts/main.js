f2e2c9fa8ab6341c80df6aaeca98c080c5b1f72d
Oleksandr Kosenko <d1izel@ukr.net>
2023-09-27 20:58:59 +0300

    fixed unscroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,3 +32,16 @@ for (const i of popupArr) {
     })
   }
 }
+
+// Unscroll only for Safari
+const body = document.body;
+const pageMenu = document.querySelector('.page__menu');
+const iconMenu = document.querySelector('.icon--menu');
+
+iconMenu.addEventListener('click', () => {
+  body.classList.add('unscroll');
+});
+
+pageMenu.addEventListener('click', () => {
+  body.classList.remove('unscroll');
+});
