68f658a60ff1d1f9f9b1bf09d557787c45ac25f6
TetianaTokar <tatyana@tokar.net.ua>
2023-09-28 13:37:23 +0300

    layout_PortPots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+/* eslint-disable */
+
+const swiper = new Swiper('.slide-content', {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  // centeredSlides: true,
+  fade: true,
+  slidesPerGroupSkip: 3,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    // dynamicBullets: true,
+  },
+});
