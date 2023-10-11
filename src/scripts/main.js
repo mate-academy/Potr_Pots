47e188e0f9d2fb33a2bb4b3bfedd620ffb6b683a
boytrend0108 <boytrend@gmail.com>
2023-10-11 18:06:47 +0300

    complete materials section 80%

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// const icons = document.querySelectorAll('.materials__icon');
+// const items = document.querySelectorAll('.materials__item-wr');
+
+// icons.forEach(icon => {
+//   icon.addEventListener('click', closeCard);
+// });
+
+// function closeCard() {
+//   items.forEach(item => {
+//     item.style.opacity = 0;
+//   });
+
+//   // icons.forEach(icon => {
+//   //   icon.style.backgroundImage
+//   //     = 'url(../images/icons/icon-materials.png)';
+//   // });
+// }
