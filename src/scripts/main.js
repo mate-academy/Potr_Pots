08a4de31a9719d7bfb67c92a44dfe5c153c19343
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-10-16 02:39:10 +0300

    Potr Pots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+if (window.location.hash === '#menu') {
+  document.body.classList.add('page__body--menu');
+}
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
+const form = document.querySelector('.questions__form');
+const inputs = document.querySelectorAll('.questions__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
