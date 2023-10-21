88a8ec65432fb899cf538c811e392c4ff6d5cf08
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-10-21 06:45:29 +0300

    all is fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,3 +22,44 @@ form.addEventListener('submit', (event) => {
     input.value = '';
   });
 });
+
+window.onload = () => {
+  let openSlide = null;
+
+  const isTabletOrLarger = window.innerWidth >= 640;
+
+  for (let i = 1; i <= 5; i++) {
+    const input = document.getElementById(`slide${i}`);
+    const label = document.querySelector(`.materials__label--${i}`);
+    const slide = document.querySelector(`.materials__slide--${i}`);
+
+    if (i === 1) {
+      input.checked = true;
+      slide.style.display = 'block';
+      label.classList.add('active');
+      openSlide = slide;
+    }
+
+    label.addEventListener('click', () => {
+      if (openSlide !== slide) {
+        if (openSlide !== null) {
+          openSlide.style.display = 'none';
+
+          const prevLabel = document.querySelector('.materials__label.active');
+
+          prevLabel.classList.remove('active');
+        }
+
+        input.checked = true;
+        slide.style.display = 'block';
+        label.classList.add('active');
+        openSlide = slide;
+      } else if (isTabletOrLarger) {
+        input.checked = false;
+        slide.style.display = 'none';
+        label.classList.remove('active');
+        openSlide = null;
+      }
+    });
+  }
+};
