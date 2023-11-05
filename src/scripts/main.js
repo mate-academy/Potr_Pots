963e09a1f049a080418fb0c834ba08e611f436c6
Anna <annashcheglova.06.08@gmail.com>
2023-11-05 19:57:49 +0200

    potr pots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+window.onload = function() {
+  [...document.getElementsByClassName('circle-button')].forEach(el => {
+    el.addEventListener('click', function() {
+      const currentButton = this;
+
+      const symbolVerical = currentButton.childNodes.item(1);
+
+      currentButton.classList.forEach(function(className) {
+        if (!className.indexOf('circle-button--')) {
+          const numberOfButton = className.substr(className.length - 1);
+
+          window.console.log(numberOfButton);
+
+          const cardInfo = document
+            .getElementsByClassName('cardinfo--' + numberOfButton).item(0);
+
+          if (cardInfo.style['display'] === 'block') {
+            cardInfo.style['display'] = 'none';
+            currentButton.style['background-color'] = '#eb5757';
+            currentButton.style['opacity'] = '100%';
+            symbolVerical.style['display'] = 'block';
+          } else {
+            cardInfo.style['display'] = 'block';
+            currentButton.style['background-color'] = '#242323';
+            currentButton.style['opacity'] = '75%';
+            symbolVerical.style['display'] = 'none';
+          }
+
+          window.console.log(cardInfo);
+
+          return false;
+        }
+      });
+    });
+  });
+};
