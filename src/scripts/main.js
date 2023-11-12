b2c499f9b21194d08d9f2e770de4180b3415751a
BoGoBooM <MyPikcha@gmail.com>
2023-11-12 15:44:54 +0200

    cursor pointer was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,4 @@
 'use strict';
-// const imageContainer = document.querySelector('.header__img');
-
-// imageContainer.addEventListener('mousemove', (e) => {
-//   const { left, top, width, height } = imageContainer.getBoundingClientRect();
-//   const x = (e.pageX - left) / width; // Визначити відносну позицію курсора по горизонталі
-//   const y = (e.pageY - top) / height; // Визначити відносну позицію курсора по вертикалі
-
-//   imageContainer.style.setProperty('--x', x);
-//   imageContainer.style.setProperty('--y', y);
-// });
-
 
 document.addEventListener('DOMContentLoaded', () => {
   let currentSlide = 1;
