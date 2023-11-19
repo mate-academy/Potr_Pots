b49d9549f29255cfea68e427cd2832fe9bbe4c4d
Yelisieiev Kim <rbv271@outlook.com>
2023-11-19 21:11:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__input');
+
+function handleFromSubmit(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+form.addEventListener('submit', handleFromSubmit);
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('visible');
+    }
+  });
+}
+
+// eslint-disable-next-line no-undef
+const observer = new IntersectionObserver(onEntry);
+const names = document.querySelectorAll('.animation');
+
+for (const elm of names) {
+  observer.observe(elm);
+}
