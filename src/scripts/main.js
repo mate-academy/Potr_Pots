4c1311cb4a3eea77461f8255a4290f21bcfdd64e
Yelisieiev Kim <rbv271@outlook.com>
2023-11-22 12:54:44 +0200

    fixed bag, lint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,14 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__content--with-menu');
+  } else {
+    document.body.classList.remove('page__content--with-menu');
+  }
+});
+
 const form = document.querySelector('.form');
 const inputs = document.querySelectorAll('.form__input');
 
