d39b63076a3eca711e5adb25fcb533c277789bc9
“Julia <j.shkorupatska@gmail.com>
2023-12-14 13:42:54 +0200

    add materials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const bullets = document.querySelectorAll('.materials__bullet');
+
+bullets.forEach(bullet => {
+  bullet.addEventListener('click', function() {
+    bullet.classList.contains('materials__bullet--active')
+      ? bullet.classList.remove('materials__bullet--active')
+      : bullet.classList.add('materials__bullet--active');
+  });
+});
+
+// Initialize Swiper
+// eslint-disable-next-line
+const swiper = new Swiper(".materials__swiper", {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
