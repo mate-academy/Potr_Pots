8c29966aed81a1c6066aa457570e094c1ab53e49
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-01-27 21:00:34 +0100

    materials tablet update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-'use strict';
+/* 'use strict';
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#slide-menu') {
+    document.body.classList.add('content');
+  } else {
+    document.body.classList.remove('content');
+  }
+}); */
+
+function submitForm() {
+  const form = document.getElementById('form');
+
+  form.reset();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  submitForm();
+});
+
