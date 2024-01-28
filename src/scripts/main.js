07572d57f8b09777e13d7a9e71fa5706deaf5258
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-01-28 18:20:01 +0100

    issues fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
-/* 'use strict';
+ 'use strict';
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#slide-menu') {
-    document.body.classList.add('content');
+    document.body.classList.add('body__lock');
   } else {
-    document.body.classList.remove('content');
+    document.body.classList.remove('body__lock');
   }
-}); */
+});
 
 /* function submitForm() {
   const form = document.getElementById('form');
