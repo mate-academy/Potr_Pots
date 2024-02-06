fca127f87b0002f4181a969750434940e90b91c6
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-02-06 14:39:19 +0100

    header and nav responsive done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.querySelector('.header__open-button');
+  const closeButton = document.querySelector('.nav__close-button');
+  const menu = document.querySelector('.nav');
+
+  menuButton.addEventListener('click', function() {
+    menu.classList.add('is-open');
+  });
+
+  closeButton.addEventListener('click', function() {
+    menu.classList.remove('is-open');
+  });
+
+  menu.addEventListener('click', function() {
+    menu.classList.remove('is-open');
+  });
+});
