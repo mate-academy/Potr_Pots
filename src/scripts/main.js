895795aa4624529279050915c14a17b7a056e2d9
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-02-12 15:20:03 +0100

    layout finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
 'use strict';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', function() {
   const menuButton = document.querySelector('.header__open-button');
   const closeButton = document.querySelector('.nav__close-button');
   const menu = document.querySelector('.nav');
+  const materialsTab = document.querySelector('.materials__tab');
 
   menuButton.addEventListener('click', function() {
     menu.classList.add('is-open');
@@ -16,4 +19,38 @@ document.addEventListener('DOMContentLoaded', function() {
   menu.addEventListener('click', function() {
     menu.classList.remove('is-open');
   });
+
+  const closeOpenTab = (container) => {
+    const currentOpen = container.querySelector('.is-open');
+
+    if (currentOpen) {
+      currentOpen.classList.remove('is-open');
+    };
+  };
+
+  materialsTab.addEventListener('click', function(event) {
+    const target = event.target;
+    const parent = target.closest('.materials__tab-info');
+
+    if (parent) {
+      closeOpenTab(materialsTab);
+
+      const textEl = parent.querySelector('.materials__tab-info-text');
+
+      textEl.classList.add('is-open');
+    } else {
+      closeOpenTab(materialsTab);
+    }
+  });
 });
+
+const swiper = new Swiper('.materials__mob', {
+  modules: [Pagination],
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+swiper();
