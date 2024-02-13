4499d07ff1aaf1242b099b5dc9a41aee2d55e0d3
Mateusz Oślizło <amti4osl@gmail.com>
2024-02-13 11:26:19 +0100

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calculateClientWidth() {
+  document.documentElement
+    .style
+    .setProperty(
+      '--cw',
+      document.documentElement.clientWidth + 'px',
+    );
+}
+
+window.addEventListener('resize', calculateClientWidth, false);
+
+document.addEventListener('DOMContentLoaded', calculateClientWidth, false);
+
+window.addEventListener('load', calculateClientWidth);
