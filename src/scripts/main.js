82a88349673aba189ece674e293cc9e19025bef2
Fedos17 <dfkd81@gmail.com>
2024-02-23 13:05:59 +0200

    add block advantages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 import Flickity from 'flickity';
 
+// disable scrolling when the menu is open
 window.onhashchange = (e) => {
   const menu = e.newURL.includes('#menu')
     ? document.body.classList.add('page-overflow')
@@ -9,10 +10,10 @@ window.onhashchange = (e) => {
   return menu;
 };
 
+// slider
 const elem = document.querySelector('.main-carousel');
 
 const flkty = new Flickity(elem, {
-  // options
   cellAlign: 'left',
   contain: true,
   prevNextButtons: false,
@@ -22,6 +23,7 @@ flkty.on('ready', function() {
   return flkty;
 });
 
+// display of a hint text when clicking on a circle
 const circle = document.querySelectorAll('.materials__circle');
 const circleText = document.querySelectorAll('.materials__circle-text');
 const circleGrpop = document.querySelectorAll('.materials__circle-group');
@@ -46,3 +48,21 @@ for (let i = 0; i < circle.length; i++) {
     }
   });
 }
+
+// changing the button when the window size changes
+const reisizeWindow = () => {
+  const wigth = window.innerWidth;
+  const button = document.querySelector('.advantages__button');
+
+  if (wigth > 767) {
+    button.classList.remove('button--2');
+    button.classList.add('button--3');
+  } else {
+    button.classList.remove('button--3');
+    button.classList.add('button--2');
+  }
+};
+
+reisizeWindow();
+
+window.addEventListener('resize', reisizeWindow);
