e17dedb733fde8fbd546cf0f007203b73f6f77ab
Misha Siv <0Sivcov0@gmail.com>
2024-03-02 16:49:48 +0200

    add landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    menu.classList.add('.active');
+  } else {
+    menu.classList.remove('.active');
+  }
+});
+
+// const showText = (number) => {
+//   const myText = document
+//     .querySelector(`.materials__details__text-${number}`);
+//   const myBotton = document
+//     .querySelector(`.materials__details__btn-${number}`);
+//   const vertical = myBotton
+//     .querySelector('.materials__details__btn__vertical');
+
+//   myText.classList.toggle('visible');
+//   myBotton.classList.toggle('visible');
+//   vertical.classList.toggle('visible');
+// };
