30a9825d0edfbcf48828fd5fa205438c9c1b1120
Misha Siv <0Sivcov0@gmail.com>
2024-03-06 20:15:39 +0200

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,31 @@
 'use strict';
+$(document).ready(function () {
+  $('.materials__swiper').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    arrows: false,
+  });
+});
 
 const menu = document.querySelector('.menu');
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    menu.classList.add('.active');
+    menu.classList.add('active');
+    document.documentElement.style.overflow = 'hidden';
   } else {
-    menu.classList.remove('.active');
+    menu.classList.remove('active');
+    document.documentElement.style.overflow = '';
   }
 });
 
-// const showText = (number) => {
-//   const myText = document
-//     .querySelector(`.materials__details__text-${number}`);
-//   const myBotton = document
-//     .querySelector(`.materials__details__btn-${number}`);
-//   const vertical = myBotton
-//     .querySelector('.materials__details__btn__vertical');
+const showText = (number) => {
+  const myText = document.querySelector(`.materials__details__text-${number}`);
+  const myBotton = document.querySelector(`.materials__details__btn-${number}`);
+  const vertical = myBotton.querySelector('.materials__details__btn__vertical');
 
-//   myText.classList.toggle('visible');
-//   myBotton.classList.toggle('visible');
-//   vertical.classList.toggle('visible');
-// };
+  myText.classList.toggle('visible');
+  myBotton.classList.toggle('visible');
+  vertical.classList.toggle('visible');
+};
