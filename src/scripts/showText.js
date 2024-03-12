07ec965f0c67a8434f9e930fcfa0804d6acc7055
Misha Siv <0Sivcov0@gmail.com>
2024-03-12 17:28:58 +0200

    fixes

--- src/scripts/showText.js
+++ src/scripts/showText.js
@@ -5,7 +5,11 @@ const buttons = document.querySelectorAll('.materials__details__btn');
 buttons.forEach(function (button, index) {
   button.addEventListener('click', function () {
     const text = document.querySelector(`.materials__details__text-${index + 1}`);
+    const button = document.querySelector(`.materials__details__btn-${index + 1}`);
+    const vertical = button.querySelector('.materials__details__btn__vertical');
+
     button.classList.toggle('visible');
+    vertical.classList.toggle('visible');
     text.classList.toggle('visible');
   });
 });
