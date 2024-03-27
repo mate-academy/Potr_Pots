f6ecf20b57fb820b64b90c6e12674a6aaa4d87f7
Nikolay <diedaemon@gmail.com>
2024-03-27 16:42:11 +0100

    section materials complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const buttonActive ='materials__card-button--active';
+const textActive = 'materials__card-text--active';
+const buttons = page.querySelectorAll('.materials__card-button');
+const texts = page.querySelectorAll('.materials__card-text');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+
+    if (buttons[i].classList.contains(buttonActive)) {
+      buttons[i].classList.remove(buttonActive);
+      texts[i].classList.remove(textActive);
+    } else {
+
+      if (page.querySelector('.' + buttonActive)) {
+        page.querySelector('.' + buttonActive).classList.toggle(buttonActive);
+        page.querySelector('.' + textActive).classList.toggle(textActive);
+      }
+
+      buttons[i].classList.toggle(buttonActive);
+      texts[i].classList.toggle(textActive);
+    }
+  });
+}
