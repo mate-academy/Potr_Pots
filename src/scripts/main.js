f20c710e63cc555db835b169a1d669f1651983a9
Marina Semipyatnaya <epicentr50@gmail.com>
2024-03-30 21:32:56 +0200

    add task Potr Pots

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.materials__slider-button');
+  const images = document.querySelectorAll('.materials__slider-text');
+
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', function() {
+      if (images[index].style.display === 'none') {
+        images.forEach(image => {
+          image.style.display = 'none';
+        });
+        buttons.forEach(btn => btn.classList.remove('gray'));
+        images[index].style.display = 'block';
+        button.classList.add('gray');
+      } else {
+        images[index].style.display = 'none';
+        button.classList.remove('gray');
+      }
+    });
+  });
+});
