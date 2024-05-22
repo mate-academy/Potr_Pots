b4341f00967881a5c5a69e9ca7f5ded84be2aa30
Serhii Khobotov <hobbitura@gmail.com>
2024-05-22 17:33:27 +0300

    potr pots complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const card = page.querySelectorAll('.card');
+const buttons = page.querySelectorAll('.materials__button');
+const tabs = page.querySelectorAll('.tabs');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+
+    if (buttons[i].classList.contains('materials__button--active')) {
+      buttons[i].classList.remove('materials__button--active');
+      card[i].classList.remove('card--active');
+    } else {
+
+      if (page.querySelector('.materials__button--active')) {
+        page.querySelector('.materials__button--active').classList.toggle('materials__button--active');
+        page.querySelector('.card--active').classList.toggle('card--active');
+      }
+
+      buttons[i].classList.toggle('materials__button--active');
+      card[i].classList.toggle('card--active');
+    }
+  });
+}
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+});
