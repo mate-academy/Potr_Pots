2651a5b0384a0bfcb2edff486e47422011598e69
Denys Povh <kirilloffdanza@gmail.com>
2024-06-19 22:16:33 +0300

    add features content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-'use strict';
+function toggleText(index) {
+  const textBox = document.getElementById(`text-${index}`);
+  const linkButton = textBox.previousElementSibling;
+
+  if (textBox.style.display === 'none' || textBox.style.display === '') {
+    textBox.style.display = 'block';
+    linkButton.classList.add('active');
+  } else {
+    textBox.style.display = 'none';
+    linkButton.classList.remove('active');
+  }
+}
