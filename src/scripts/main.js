953ad8a5b66b0f2c3bea38cc234625fde1a58c0a
VladaProskura <vladyslava.proskura@gmail.com>
2024-06-28 13:27:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.card__item-button');
+  const images = document.querySelectorAll('.card__item-text');
+
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', function() {
+      if (images[index].style.display === 'none') {
+        images.forEach(image => {
+          image.style.display = 'none';
+        });
+        buttons.forEach(btn => btn.classList.remove('gray'));
+        images[index].style.display = 'block';
+        button.classList.add('gray');
+      } else {
+        images[index].style.display = 'none';
+        button.classList.remove('gray');
+      }
+    });
+  });
+});
