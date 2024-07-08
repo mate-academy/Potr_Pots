3b199013da8341ee6de8c7f36e48c375d7bdddfa
kyrylomanko <kyrylo.manko@gmail.com>
2024-07-08 11:48:46 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,6 +29,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+      document.body.classList.add('page__body--with-menu');
+    } else {
+      document.body.classList.remove('page__body--with-menu');
+    }
+  });
+
   inputs.forEach(function(input, index) {
     input.addEventListener('click', function() {
       if (currentIndex === index) {
